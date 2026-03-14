f9f58f11809f6a1473258f7e8e9fd06eeab31141
Ayush <aayush2005singh@gmail.com>
2026-03-14 17:24:29 +0530

    initial commit

---
+++ src/app/api/settings/get/route.ts
@@ -0,0 +1,25 @@
+import connectDB from "@/lib/db";
+import Settings from "@/model/settings.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest) {
+    try {
+        const {ownerId}=await req.json()
+        if(!ownerId){
+            return NextResponse.json(
+                {message:"owner id required"},
+                {status:400}
+            )
+        }
+        await connectDB()
+        const settings=await Settings.findOne(
+            {ownerId}
+        )
+        return NextResponse.json(settings)
+    } catch (error) {
+        return NextResponse.json(
+            {message:`get settings error ${error}`},
+            {status:500}
+        )
+    }
+}
---
+++ src/model/settings.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+import mongoose from "mongoose";
+
+interface ISettings{
+    ownerId:string
+    businessName:string
+    supportEmail:string
+    knowledge:string
+}
+
+const settingSchema=new Schema<ISettings>({
+    ownerId:{
+       type:String,
+       required:true,
+       unique:true
+    },
+    businessName:{
+       type:String
+    },
+    supportEmail:{
+       type:String
+    },
+    knowledge:{
+       type:String
+    }
+},{timestamps:true})
+
+const Settings=mongoose.models.Settings||  model("Settings",settingSchema)
+
+export default Settings
